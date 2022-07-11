964656ec9bcc76d9c926ddb25e17dcca3bd2251b
ndj <dongjin.nam@kt.com>
2022-07-11 22:08:23 +0900

    MOD: app.js (message)

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello Nandy! AWS CICD Tutorial Completed'));
+app.get('/', (req, res) => res.send('Hello Nandy! AWS CICD Tutorial Completed!!!!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
